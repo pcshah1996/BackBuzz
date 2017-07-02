890f2c5a2eeb857fd7c1ef14508dfe64d3557510
Prateek Shah <pcshah1996@gmail.com>
2017-07-01 22:28:27 -0700

    Add simple assets

--- BackBuzz/ViewController.swift
+++ BackBuzz/ViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import AudioToolbox.AudioServices
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var timeCount: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var onOffText: UILabel!
+    @IBOutlet weak var timeSlider: UISlider!
+    @IBOutlet weak var activateBuzz: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: Actions
+    @IBAction func timeSlider(_ sender: UISlider, forEvent event: UIEvent) {
+        var currentValue = Int(sender.value)
+        timeCount.text = "\(currentValue)"
+        AudioServicesPlayAlertSound(kSystemSoundID_Vibrate);
+    }
 
+    @IBAction func activateBuzz(_ sender: UISwitch, forEvent event: UIEvent) {
+    }
 }
 
